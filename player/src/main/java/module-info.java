499490ea7046cf97155d4de35a0fe72a3afa4827
palminjae <palminjae@gmail.com>
2025-06-07 15:45:58 +0900

    fix: PlayerModule 최소한 구현으로 빌드 오류 해결

--- player/src/main/java/module-info.java
+++ player/src/main/java/module-info.java
@@ -1,15 +1,6 @@
-// player/src/main/java/module-info.java
-
 module synctune.player.main {
-    // SDK 의존성
     requires synctune.sdk.main;
-    
-    // Java Desktop API (javax.sound.sampled 사용을 위해 필요)
-    requires java.desktop;
-    
-    // 로깅
     requires org.slf4j;
     
-    // 패키지 내보내기 (다른 모듈에서 접근해야 하는 경우)
     exports ac.cwnu.synctune.player;
 }
