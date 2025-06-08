959c29296e2328e16e16e2eb63e8d134f311a149
MS.Lim <beewalle@naver.com>
2025-06-08 18:21:36 +0900

    fix(ui): playlist

--- ui/src/main/java/ac/cwnu/synctune/ui/controller/PlaylistActionHandler.java
+++ ui/src/main/java/ac/cwnu/synctune/ui/controller/PlaylistActionHandler.java
@@ -3,6 +3,8 @@ package ac.cwnu.synctune.ui.controller;
 import ac.cwnu.synctune.sdk.event.EventPublisher;
 import ac.cwnu.synctune.sdk.event.PlaylistEvent;
 import ac.cwnu.synctune.ui.view.PlaylistView;
+import ac.cwnu.synctune.sdk.playlist.Playlist;
+import ac.cwnu.synctune.sdk.playlist.PlaylistManager;
 
 /**
  * PlaylistView에서 발생하는 사용자 액션을 처리하고
@@ -11,41 +13,53 @@ import ac.cwnu.synctune.ui.view.PlaylistView;
 public class PlaylistActionHandler {
     private final PlaylistView view;
     private final EventPublisher publisher;
+    private final PlaylistManager playlistManager;
 
     /**
      * 생성자
      * @param view PlaylistView 인스턴스
      * @param publisher 이벤트 발행을 위한 EventPublisher
      */
-    public PlaylistActionHandler(PlaylistView view, EventPublisher publisher) {
+    public PlaylistActionHandler(PlaylistView view, EventPublisher publisher, PlaylistManager playlistManager) {
         this.view = view;
         this.publisher = publisher;
+        this.playlistManager = playlistManager;
         attachEventHandlers();
     }
 
     private void attachEventHandlers() {
-        view.getCreateButton().setOnAction(e -> 
-            publisher.publish(new PlaylistEvent.PlaylistCreatedEvent(view.getPlaylistNameInput()))
-        );
+        view.getCreateButton().setOnAction(e -> {
+            String name = view.getPlaylistNameInput();
+            Playlist playlist = playlistManager.createPlaylist(name);
+            publisher.publish(new PlaylistEvent.PlaylistCreatedEvent(playlist));
+        });
 
-        view.getDeleteButton().setOnAction(e -> 
-            publisher.publish(new PlaylistEvent.PlaylistDeletedEvent(view.getSelectedPlaylist()))
-        );
+        view.getDeleteButton().setOnAction(e -> {
+            String name = view.getSelectedPlaylist();
+            Playlist playlist = playlistManager.getPlaylistByName(name);
+            publisher.publish(new PlaylistEvent.PlaylistDeletedEvent(playlist));
+        });
 
-        view.getAddButton().setOnAction(e -> 
+        view.getAddButton().setOnAction(e -> {
+            String name = view.getSelectedPlaylist();
+            Playlist playlist = playlistManager.getPlaylistByName(name);
             publisher.publish(new PlaylistEvent.MusicAddedToPlaylistEvent(
-                view.getSelectedPlaylist(), view.getSelectedMusic()))
-        );
+                playlist, view.getSelectedMusic()));
+        });
 
-        view.getRemoveButton().setOnAction(e -> 
+        view.getRemoveButton().setOnAction(e -> {
+            String name = view.getSelectedPlaylist();
+            Playlist playlist = playlistManager.getPlaylistByName(name);
             publisher.publish(new PlaylistEvent.MusicRemovedFromPlaylistEvent(
-                view.getSelectedPlaylist(), view.getSelectedMusic()))
-        );
+                playlist, view.getSelectedMusic()));
+        });
 
         // 재생목록 순서 변경 시 이벤트 발행
-        view.getPlaylistItems().addListener((obs, oldList, newList) -> 
+        view.getPlaylistItems().addListener((obs, oldList, newList) -> {
+            String name = view.getSelectedPlaylist();
+            Playlist playlist = playlistManager.getPlaylistByName(name);
             publisher.publish(new PlaylistEvent.PlaylistOrderChangedEvent(
-                view.getSelectedPlaylist(), view.getCurrentOrder()))
-        );
+                playlist, view.getCurrentOrder()));
+        });
     }
 }
